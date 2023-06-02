8ddc848e94bf5902fade95002243e111e82426b3
Si Si <96635252+sssssissssss@users.noreply.github.com>
2023-06-02 05:38:40 +0300

    add bash

---
+++ Dependencies.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Downloading Python libraries...
+pip install PyQt5==5.15.4
+pip install nltk==3.6.5
+pip install pyarabic==0.6.14
+pip install camel-tools==1.2.0
+echo Libraries downloaded successfully.
+pause
